935b04d8e48bfc2f648bb54fca62eed4b184886d
vinicius emanuel <vncemanuel@gmail.com>
2021-04-21 15:17:02 -0300

    timer interval for countdown

--- alarm-clock/alarm-clock/ViewControllers/AlarmClockViewController.swift
+++ alarm-clock/alarm-clock/ViewControllers/AlarmClockViewController.swift
@@ -8,19 +8,20 @@
 import UIKit
 
 class AlarmClockViewController: UIViewController {
-
+    
     @IBOutlet weak var pickerView: UIPickerView!
     @IBOutlet weak var timerLabel: UILabel!
     @IBOutlet weak var clockImage: UIImageView!
     @IBOutlet weak var timerButton: UIButton!
     
-    let options = ["5 segundos" ,"10 segundos", "15 segundos", "20 segundos"]
-    
-    var timerStaterd = false
+    private let options = ["--", "5 segundos" ,"10 segundos", "15 segundos", "20 segundos"]
+    private var interval: Int = 0
+    private var timerStaterd = false
+    private var timer: Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         self.pickerView.delegate = self
         self.pickerView.dataSource = self
     }
@@ -30,7 +31,7 @@ class AlarmClockViewController: UIViewController {
         self.showTimer(started: self.timerStaterd)
         self.showClockImage(size: self.view.frame.size)
     }
-
+    
     static func instance() -> AlarmClockViewController {
         let id = "AlarmClockViewController"
         let storyboard = UIStoryboard(name: "AlarmClock", bundle: nil)
@@ -45,6 +46,37 @@ class AlarmClockViewController: UIViewController {
     @IBAction func buttonTapped(_ sender: UIButton) {
         self.timerStaterd.toggle()
         self.showTimer(started: self.timerStaterd)
+        if self.timerStaterd {
+            self.startTimer()
+        } else {
+            self.stopTime()
+        }
+    }
+    
+    private func stopTime() {
+        self.timer?.invalidate()
+        self.timerLabel.text = "00:00"
+        self.interval = 0
+        self.pickerView.selectRow(0, inComponent: 0, animated: false)
+    }
+    
+    private func startTimer() {
+        if self.interval == 0 { return }
+        
+        self.timer = Timer(fire: Date(), interval: 1, repeats: true, block: { [unowned self] (_) in
+            
+            if self.interval  == 0 {
+                self.timer?.invalidate()
+                self.timerLabel.text = "00:00"
+            } else {
+                self.timerLabel.text = self.interval < 10 ? "00:0\(self.interval)" : "00:\(self.interval)"
+            }
+            
+            self.interval -= 1
+        })
+        if let timer = self.timer {
+            RunLoop.current.add(timer, forMode: .common)
+        }
     }
     
     private func showClockImage(size: CGSize) {
@@ -65,9 +97,13 @@ class AlarmClockViewController: UIViewController {
 }
 
 extension AlarmClockViewController: UIPickerViewDelegate, UIPickerViewDataSource {
-   
+    
     func numberOfComponents(in pickerView: UIPickerView) -> Int { 1 }
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int { self.options.count }
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? { self.options[row] }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.interval = row * 5
+    }
 }
 
